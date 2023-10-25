c368aec2097f36f2e0f91dfd0bccfcfaec8569a6
MuriloAlvesGD <luriomgdevelop@gmail.com>
2023-10-25 10:30:56 -0300

    Projeto Base

---
+++ src/main/java/com/api/pixcontrol/PixControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.pixcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/pixcontrol/PixControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.pixcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
